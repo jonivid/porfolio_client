49991de832b55dbbf7c2d4a16836ac1dda462138
jonivid <yonivid@gmail.com>
2024-09-15 18:27:29 +0300

    refactored porfolio

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // app/page.js
 import ProjectCard from "../../components/projectCard/ProjectCard";
 
@@ -21,36 +22,30 @@ export default function page() {
       id: 3,
       title: "Project 1",
       description: "A brief description of Project 1",
-      previewImage: "/assets/website_ui_pic.jpg", // Replace with your actual preview image path
-      videoSrc: "/assets/website_ui.mp4", // Replace with your actual video path
+      previewImage: "/assets/website_ui_pic1.jpg", // Replace with your actual preview image path
+      videoSrc: "/assets/website_ui1.mp4", // Replace with your actual video path
     },
     {
       id: 4,
       title: "Project 2",
       description: "A brief description of Project 2",
-      previewImage: "/assets/website_ui_b&w_pic.jpg", // Replace with your actual preview image path
-      videoSrc: "/assets/website_ui_B&W.mp4", // Replace with your actual video path
+      previewImage: "/assets/website_ui_pic2.jpg", // Replace with your actual preview image path
+      videoSrc: "/assets/website_ui2.mp4", // Replace with your actual video path
     },
-    {
-      id: 5,
-      title: "Project 1",
-      description: "A brief description of Project 1",
-      previewImage: "/assets/website_ui_pic.jpg", // Replace with your actual preview image path
-      videoSrc: "/assets/website_ui.mp4", // Replace with your actual video path
-    },
-    {
-      id: 6,
-      title: "Project 2",
-      description: "A brief description of Project 2",
-      previewImage: "/assets/website_ui_b&w_pic.jpg", // Replace with your actual preview image path
-      videoSrc: "/assets/website_ui_B&W.mp4", // Replace with your actual video path
-    },
+    
   ];
 
   return (
     <div className="min-h-screen bg-gray-800 py-12 px-4 flex flex-col items-center">
       <h1 className="text-4xl font-bold text-white mb-10">My Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <h1 className="text-4xl font-bold text-gray-900 mb-6">
+        Check out my latest work
+      </h1>
+      <p className="text-lg text-gray-700 mb-4">
+        I've worked on a variety of projects, from simple websites to complex
+        web applications. Here are a few of my favorites.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
